5f9a4f73a9ff38c66dc51ee614d3936eed2d7c2b
Toribio <toribio.rodriguez@hotmail.com>
2021-01-29 18:17:45 +0100

    Add Console Aplication for test and working with files

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(@".\hello.txt", "Hello World");
+            foreach (string file in Directory.GetFiles(@".\"))
+            {
+                Console.WriteLine(file);
+            }
+            string[] files = Directory.GetFiles(@".\", "*", SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+        }
+    }
+}
